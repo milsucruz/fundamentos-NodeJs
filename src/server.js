09060fa6d322d2d8462695b48803c85c70d120f3
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-08-01 10:47:33 -0300

    HTTP methods

--- src/server.js
+++ src/server.js
@@ -1,7 +1,34 @@
 import http from 'node:http'
 
+// - Criar usuários
+// - Listar usuários
+// - Editar usuários
+// - Remover usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, PUT, POST, PATHC, DELETE 
+
+// - GET => Buscar uma recurso do back-end
+// - POST => Criar uma recurso no back-end
+// - PUT => Editar / atualizar uma recurso no back-end
+// - PATCH => Atualizar uma informação específica de um recurso no back-end
+// - DELETE => Deletar um recurso do back-end
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello World')
+  const {method, url} = req;
+
+  if(method === 'GET' && url === '/users') {
+    return res.end('Lista de usuários');
+  } 
+
+  if(method === 'POST' && url === '/users') {
+    return res.end('Criando usuário');
+  }
+  
+  return res.end('Hello world')
 })
 
 server.listen(3333)
