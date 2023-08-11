07f60ca2b36df2174a1304ee86c38b36e78bf220
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-08-11 14:40:42 -0300

    Remove register

--- src/database.js
+++ src/database.js
@@ -6,12 +6,13 @@ export class Database {
   #database = {}
 
   constructor() {
-    fs.readFile(databasePath, 'utf8').then(data => {
-      this.#database = JSON.parse(data)
-    })
-    .catch(() => {
-      this.#persist()
-    })
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
   }
 
   #persist() {
@@ -28,11 +29,20 @@ export class Database {
     if (Array.isArray(this.#database[table])) {
       this.#database[table].push(data)
     } else {
-      this.#database[table] = data
+      this.#database[table] = [data]
     }
 
     this.#persist()
 
     return data
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,17 @@
-import { Database } from './database.js';
-import { randomUUID } from 'node:crypto';
-import { buildRoutePath } from './utils/build-route-path.js';
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
-
-const database = new Database
+const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
     path: buildRoutePath('/users'),
     handler: (req, res) => {
-      const  users = database.select('users')
+      const users = database.select('users')
 
-      return res
-      .end(JSON.stringify(users));
+      return res.end(JSON.stringify(users))
     }
   },
   {
@@ -27,15 +25,21 @@ export const routes = [
         name,
         email,
       }
-  
+
       database.insert('users', user)
-  
+
       return res.writeHead(201).end()
     }
   },
   {
-    method: 'DELETE',	
+    method: 'DELETE',
     path: buildRoutePath('/users/:id'),
-    handler: (req, res) => {},
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
+    }
   }
 ]
