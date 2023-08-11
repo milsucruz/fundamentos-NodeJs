ea89dd958690ade5e4ef1f3f6d51efed71beab3a
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-08-11 11:00:03 -0300

    regex

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,5 +1,8 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-  return new RegExp()
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+
+  return pathRegex
 }
